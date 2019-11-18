85ad6f6082e24e382a5fbcc10047eff37cc9b3bf
castares <cesar.castares@gmail.com>
2019-11-18 20:07:53 +0100

    working on it

--- src/google_api_requests.py
+++ src/google_api_requests.py
@@ -8,29 +8,33 @@ load_dotenv()
 #API authentication
 api_key = os.getenv("GOOGLE_CLOUD_API_KEY")
 
-def nearbyRequest(longitude, latitude, radius):
+def nearbyRequest(longitude, latitude, radius, place_type):
     url = f"https://maps.googleapis.com/maps/api/place/nearbysearch/json?"
     params = {
         'location' : f'{latitude},{longitude}',
         'radius' : radius,
-        'key' : api_key
+        'key' : api_key,
+        'type' : place_type
     }
-    answers = []
+    places = []
     r = requests.get(url, params=params).json()
-    answers.append(r)
+    extract = list(map(lambda x: places.append(x), r['results'] ))
     while ('next_page_token' in r):
         time.sleep(2)
-        params['pageToken'] = r['next_page_token']
+        params['pagetoken'] = r['next_page_token']
         r = requests.get(url, params=params).json()
-        answers.append(r)
-    print(answers)
-    return answers
+        extract = list(map(lambda x: places.append(x), r['results'] ))
+    return places
+
+
 
 def main():
     longitude = -0.208979 
     latitude = 51.51819
-    radius=200
-    return nearbyRequest(longitude, latitude, radius)
+    radius=1500
+    place_type='cafe'
+    places = nearbyRequest(longitude, latitude, radius, place_type)
+
 
 if __name__=="__main__":
     main()
